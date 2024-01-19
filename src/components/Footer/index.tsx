7986b97d86c055804eac39a3770c718ca5f68b51
Mohaimin Islam <mohaiminislam94@gmail.com>
2024-01-18 20:59:56 -0500

    chore: refactor code

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,26 +1,10 @@
-import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa6";
-import { FaStackOverflow } from "react-icons/fa";
-import { IoIosMail } from "react-icons/io";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { socialMediaOptions } from "./options";
 
-const socialMedia = [
-  {
-    name: "LinkedIn",
-    link: "https://www.linkedin.com/in/mohaimin-islam/",
-    icon: <FaLinkedin />,
-  },
-  {
-    name: "StackOverflow",
-    link: "https://stackoverflow.com/users/8687318/mohaimin",
-    icon: <FaStackOverflow />,
-  },
-  {
-    name: "Email",
-    link: "mailto:mohaimin.islam94@gmail.com",
-    icon: <IoIosMail />,
-  },
-];
+const linkBaseClassName =
+  "px-4 text-slate-500 xl:px-5 h-full flex justify-center items-center hover:text-primary transition-colors ease-in-out duration-200";
 
 export default function Footer() {
   return (
@@ -32,16 +16,16 @@ export default function Footer() {
         <h2 className="py-3 border-r border-border xl:pr-4 pr-2  max-sm:grow">
           Find me in:
         </h2>
-        <div className="flex flex-row justify-start items-center h-full w-auto">
-          {socialMedia.map((item) => (
+        <div className="flex flex-row justify-start items-center h-full">
+          {socialMediaOptions.map((socialMediaOption) => (
             <Link
-              key={item.name}
-              href={item.link}
+              key={socialMediaOption.name}
+              href={socialMediaOption.link}
               target="_blank"
               rel="noopener noreferrer"
-              className="px-4 xl:px-5 border-r border-r-border h-full flex justify-center items-center hover:text-amber-500 transition-colors ease-in-out duration-200"
+              className={cn(linkBaseClassName, "border-r border-r-border ")}
             >
-              {item.icon}
+              {socialMediaOption.icon}
             </Link>
           ))}
         </div>
@@ -50,7 +34,7 @@ export default function Footer() {
         href="https://github.com/SL0TR"
         target="_blank"
         rel="noopener noreferrer"
-        className="px-4 xl:px-5  border-l border-l-border h-full flex justify-center items-center hover:text-amber-500 transition-colors ease-in-out duration-200"
+        className={cn(linkBaseClassName, "border-l border-l-border ")}
       >
         <p className="mr-2 hidden xl:block">@mohaimin</p>
         <FaGithub />
---
+++ src/components/Footer/options.tsx
@@ -0,0 +1,20 @@
+import { FaLinkedin, FaStackOverflow } from "react-icons/fa";
+import { IoIosMail } from "react-icons/io";
+
+export const socialMediaOptions = [
+  {
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/mohaimin-islam/",
+    icon: <FaLinkedin />,
+  },
+  {
+    name: "StackOverflow",
+    link: "https://stackoverflow.com/users/8687318/mohaimin",
+    icon: <FaStackOverflow />,
+  },
+  {
+    name: "Email",
+    link: "mailto:mohaimin.islam94@gmail.com",
+    icon: <IoIosMail />,
+  },
+];
