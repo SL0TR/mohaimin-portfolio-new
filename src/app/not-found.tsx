000f9fb7375c35cb9fede8fdfa40fc6a625d0bb8
Mohaimin Islam <mohaiminislam94@gmail.com>
2024-01-27 15:34:06 -0500

    chores: - add global not found page - add alert comp

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import GlobalAlert from "@/components/GlobalAlert";
+
+export default function Notfound() {
+  return (
+    <section className="grow">
+      <div className="flex justify-center items-center h-full w-full">
+        <GlobalAlert />
+      </div>
+    </section>
+  );
+}
